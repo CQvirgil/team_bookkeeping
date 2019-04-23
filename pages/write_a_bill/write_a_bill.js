34a87f845508c6b600330dccd9ea737e19e08836
陈为钦 <1453226913@qq.com>
2019-04-23 17:16:35 +0800

    添加记账页面的付款人选择弹窗和账单内容弹窗

--- pages/write_a_bill/write_a_bill.js
+++ pages/write_a_bill/write_a_bill.js
@@ -16,9 +16,53 @@ Page({
     text_date: '今天',
     end_date: '',
     isShowInput: true,
+    isShowDialog1: false,
     person_name: '',
     person_headimg: '',
     money: 0,
+    bill_content:'一般',
+    radio_button_style: 'radio-button',
+    dialog_input_length: 0,
+    dialog_input_text: '',
+    dialgo_animation: null,
+    isShowPeopleDialog: false,
+    radio_button_data: [{
+        id: 1,
+        value: '一般',
+        checked: false,
+        radio_button_style: 'radio-button-check'
+      },
+      {
+        id: 2,
+        value: '餐饮',
+        checked: false,
+        radio_button_style: 'radio-button'
+      },
+      {
+        id: 3,
+        value: '住宿',
+        checked: false,
+        radio_button_style: 'radio-button'
+      },
+      {
+        id: 4,
+        value: '交通',
+        checked: false,
+        radio_button_style: 'radio-button'
+      },
+      {
+        id: 5,
+        value: '门票',
+        checked: false,
+        radio_button_style: 'radio-button'
+      },
+      {
+        id: 6,
+        value: '购物',
+        checked: false,
+        radio_button_style: 'radio-button'
+      }
+    ]
   },
 
 
@@ -58,16 +102,108 @@ Page({
       delta: 4
     })
   },
+  //单选按钮点击事件
+  radio_check: function(e) {
+    console.log(e.currentTarget.dataset.id)
+    var data = this.data.radio_button_data
+    var text = ''
+    //遍历按钮数据
+    for (var i = 0; i < this.data.radio_button_data.length; i++) {
+      //判断传回的id是否等于当前遍历的id
+      if (e.currentTarget.dataset.id == this.data.radio_button_data[i].id) {
+        //设置当前遍历的数据为已点击状态
+        data[i].checked = true
+        text = data[i].value
+      } else {
+        data[i].checked = false
+      }
+      //判断是否为点击状态
+      if (data[i].checked) {
+        data[i].radio_button_style = 'radio-button-check'
+      }else{
+        data[i].radio_button_style = 'radio-button'
+      }
+      this.setData({
+        radio_button_data: data,
+        bill_content: text
+      })
+    }
+  },
+
+  dialog_input: function(e){
+    var text = e.detail.value
+    this.setData({
+      dialog_input_length: text.length,
+      dialog_input_text:text
+    })
+  },
+
+  clicktBillContent:function(e) {
+    wx.setNavigationBarColor({
+      frontColor: '#000000',
+      backgroundColor: '#7f7f7f',
+    })
+    var animation = wx.createAnimation({
+      delay: 10000,
+      duration: 40000,
+      timingFunction: 'ease',
+    })
+    animation.translateY(0).step()
+    this.setData({
+      isShowInput: false,
+      isShowDialog1: true,
+      dialgo_animation: animation.export(),
+    })
+  },
+
+  clickDialogQueding: function (e) {
+    wx.setNavigationBarColor({
+      frontColor: '#000000',
+      backgroundColor: '#ffffff',
+    })
+    if (this.data.dialog_input_text.length!=0){
+      var text = this.data.dialog_input_text
+      this.setData({
+        bill_content: text
+      })
+    }
+    console.log(this.data.dialog_input_text.length)
+    this.setData({
+      isShowInput: true,
+      isShowDialog1: false
+    })
+  },
+
+  clickPeopleItem: function(e){
+    wx.setNavigationBarColor({
+      frontColor: '#000000',
+      backgroundColor: '#ffffff',
+    })
+    this.setData({
+      isShowInput: true,
+      isShowPeopleDialog: false
+    })
+  },
+  clickPayer:function(e){
+    wx.setNavigationBarColor({
+      frontColor: '#000000',
+      backgroundColor: '#7f7f7f',
+    })
+    this.setData({
+      isShowInput: false,
+      isShowPeopleDialog: true
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function(options) {
-    this.setData({
-      date: util.formatTime(),
-      end_date: util.formatTime(),
-      person_name: app.globalData.userInfo.nickName,
-      person_headimg: app.globalData.userInfo.avatarUrl
-    })
+    // this.setData({
+    //   date: util.formatTime(),
+    //   end_date: util.formatTime(),
+    //   person_name: app.globalData.userInfo.nickName,
+    //   person_headimg: app.globalData.userInfo.avatarUrl
+    // })
   },
 
   /**
